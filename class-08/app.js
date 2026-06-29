e576a23f4c6ebed98be34bb9cbc1e887b94888bc
Tuba282 <tubajan282@gmail.com>
2026-06-29 10:49:48 +0500

    JS start Variables + DataTypes

---
+++ class-08/app.js
@@ -0,0 +1,105 @@
+
+// console.log("Hello Promming");
+
+
+// console.log("Humna Arnib");
+// // console.log("Humna Arnib");
+
+// // single line comment --- //
+// // multi line comment /*-------*/
+
+// /*console.log("Humna Arnib");
+// console.log("Humna Arnib");
+
+// */
+// console.log("Tuba Jan");
+
+
+
+
+// --- DataTypes ---
+
+
+// number -- Number 
+// characters-- abgy 123 !@#$%^&*()  --  String
+// yes/no --- Boolean 
+
+
+
+// number -- Number 
+// console.log(5 + 3);
+
+
+
+// ---  String --- "" ''  
+
+// console.log("Ayesha");
+// console.log('Aliya Zahra');
+
+
+// Boolean -true / false -- presence / absence 
+
+console.log(true);
+console.log(false);
+
+
+
+
+// testing
+
+// write a string ("") of single line sentence that give output (Browser) to console (inspect)
+
+// console.log("Aliya is a good girl");
+
+// --- Variables --- Containers
+
+//  Containers -- daily life save
+
+
+// Kitchen -- masalay -- 
+
+//  variable syntax
+
+// let variableName = value
+
+// let name = "Yushfa";
+// console.log(name);
+
+
+// Strings -- variables
+
+let firstDibba = "Haldi";
+let secondDibba = "ChatMasala";
+let thirdDibba = "LalMirch";
+let forthDibba = "Biryani Masala";
+
+console.log(secondDibba);
+
+
+
+// --Numbers ---
+
+let DuaNumber = 8867060
+let MyNumber  = 5765675
+// console.log(MyNumber);
+
+// - Bollean -- true / false
+
+let isParent = false;
+
+
+// Student Name = Ali Ahmed
+// Age = 24
+// isPresent -- yes
+
+let student_Name = "Ali Ahmed"
+let age = 24
+let ispresent = true
+
+console.log(student_Name);
+console.log(age);
+console.log(ispresent);
+
+
+
+
