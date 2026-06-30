047d810917471268982541837f8956108670c0f2
Tuba282 <tubajan282@gmail.com>
2026-06-30 10:28:59 +0500

    comparision operators + conditions

---
+++ class-09/app.js
@@ -0,0 +1,86 @@
+// output browser 
+
+
+// console.log("JS-Class-02");
+// testing
+// debugging 
+
+
+// single line comment --> //
+// multi line comment /*-----*/
+
+
+// ---- Datatypes ----
+
+// String
+// Number
+// Boolean
+
+
+// Variables
+
+// let className = "CLASS_09";
+// let no_Students = 25;
+// let wePresent = true;
+
+
+// console.log(className);
+// console.log(no_Students);
+// console.log(wePresent);
+
+
+// console.log(`I am taking ${className} and we are total ${no_Students} and yes we all are present , this is ${wePresent}`);
+
+// ` --> templete litterals
+
+
+// -------------------
+// -------------------
+// -------------------
+// -------------------
+
+//  Comparision Operators ---> return Boolean 
+
+//  < > <= >= == === != !==
+
+// console.log(5 > 5);//false
+// console.log(5 < 5);//false
+// console.log(5 >= 5);//true
+// console.log(5 == '5');//true
+// console.log(5 === '5');//true
+
+// let food = "Biryani"
+// let choise = "Daal Chawal"
+
+// if (food == choise) {
+//     alert(`Mummy ${food} Kha rahi hain.`);
+
+// } else  {
+//     alert(`Mummy ${choise} Kha rahi hain.`);
+// }
+
+
+// marks > 90 == A1 grade
+// marks > 80 == A grade
+// marks > 70 == B grade
+// marks > 60 == C grade
+// marks > 50 == D grade
+// otherwise == Fail
+
+
+let marks = 78
+
+if (marks > 90) {
+    console.log(`Grade = A1`);
+} else if (marks > 80) {
+    console.log(`Grade = A`);
+} else if (marks > 70) {
+    console.log(`Grade = B`);
+} else if (marks > 60) {
+    console.log(`Grade = C`);
+} else if (marks > 50) {
+    console.log(`Grade = D`);
+} else {
+    console.log(`Fail`);
+}
+
