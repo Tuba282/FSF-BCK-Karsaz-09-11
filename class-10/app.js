9caf5f91f9f987c53f2070c7580dd6beba6bbe2d
Tuba282 <tubajan282@gmail.com>
2026-07-06 10:10:40 +0500

    for loop

---
+++ class-10/app.js
@@ -0,0 +1,59 @@
+// ---- Loops ----------
+
+// Loops -- repeatition --
+// Types --- main 3 types 
+// -------For Loop
+// ------While Loop
+// ---   Do-While Loop
+
+
+// ------ Foor Loop ------------
+// --- Counter Loop --- 
+// known Repeatition ---
+
+
+// for (starting ; condition ; update){
+//     // Block of Code
+// }
+
+
+// let a  = 1;// initialization;
+// a > 10 //  condition 
+// a = a + 1 ///-- 3 update
+// a = a - 1 ///---1 update
+
+
+// for(let a = 1 ; a <= 5 ; a = a + 1 ){ // a = 6
+//     console.log("Ayesha" , a);
+// }
+// console.log("loop End");
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+
+//  Write a table of 5 using for loop 
+//  5 x 1 = i*a (5)
+//  5 x 2 = i*a (10)
+//  5 x 3 = i*a (15)
+
+let table = 7;
+
+for (let i = 1; i <= 10; i = i + 1) {
+    console.log(`${table} x ${i} =  ${i * table}`);
+    //  7 14  ---- 
+}
+
+
+// step - 1 - start  (1)
+// step - 2 - condition match 
+// step - 3 - curly braces -- execute
+// step - 4 - update
