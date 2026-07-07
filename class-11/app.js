7337ce697b1d775b5bd1a85d5efd2729f6531a9d
Tuba282 <tubajan282@gmail.com>
2026-07-07 10:23:37 +0500

    for loop + while loop + table generator

---
+++ class-11/app.js
@@ -0,0 +1,56 @@
+// --- For loop --- Counter ----
+// to get userInput in JS we use Prompt ------
+
+
+// let table = prompt("Enter  Table Number ");
+
+// for(let i = 1 ; i<= 10 ; i = i+1){
+//     console.log(`${table} x ${i} = ${i * table}`);
+//     // 7 x 1 =  i * table (1x7) = 7
+//     // 7 x 2 =  i * table (2x7) = 14
+//     // 7 x 3 =  i * table (3 x 7) = 21
+
+//     document.write(`<div class="table">${table} x ${i} = ${i * table}</div>`)
+
+// }
+
+// ---- While Loop ----
+// ---- Conditional Loop ----
+
+// initialization
+// condition
+// block of code 
+// update
+
+
+
+
+
+// let i = 0;
+// while (condition) {
+//     // Block of Code
+//     // Block of Code
+//     // Block of Code
+//     // Block of Code
+//     // Block of Code
+
+//     // updatation
+// }
+
+
+
+// let startPoint = prompt("Enter Starting With ")
+// let endPoint = prompt("Enter Ending With ")
+
+
+
+let table = prompt("Enter Table Number ")
+let i = 1;//initialization
+
+while (i <= 10) {//COndition
+    document.write(`<div>${table} x ${i} = ${i * table}</div>`)
+    // Block of Code
+
+
+    i = i + 1//update
+}
