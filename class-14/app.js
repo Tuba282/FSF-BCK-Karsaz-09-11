2e37d5bfa6371bde8ff5922066b1003a3e48c6eb
Tuba282 <tubajan282@gmail.com>
2026-07-20 09:25:46 +0500

    JS-quiz-01 -- array start

---
+++ class-14/app.js
@@ -0,0 +1,18 @@
+// ---- arrays -----
+// jub single location pe multiple data data save karwana ho , to Arrays 
+
+let name = "Ali"
+let names = ["Ali", "Usama", "Kashan", "Ahmed", "Haroon" , "Sufyan", "Atif"]
+let fruits = ["Apple", "Banana", "Mango", "Strawberray", "PineApple", "WaterMelon", "Grapes"]
+
+// ---1st- array index wise start with - 0
+// ---2nd- array length wise start with - 1
+
+
+// console.log(names[3]);
+// console.log(names[1]);
+// // array ke element ko get karne ke liye Index(start with 0) wise count karna
+
+// console.log(names[4]);
+
+console.log(`${names[4]} loves ${fruits[3]}`);
